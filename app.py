8c4f2dade07d4de2e70ef94e5af2d013049f2810
m0cchi <boom.boom.planet@gmail.com>
2018-07-28 23:39:43 +0900

    keep messages

--- app.py
+++ app.py
@@ -3,24 +3,36 @@ from bottle.ext.websocket import GeventWebSocketServer
 from bottle.ext.websocket import websocket
 import threading
 from kafka import KafkaConsumer, KafkaProducer
+import redis
 
 users = set()
 
 producer = KafkaProducer(bootstrap_servers='kafka1:9092')
+#uncommit_consumer = KafkaConsumer('chat', auto_offset_reset='earliest', enable_auto_commit=False, bootstrap_servers='kafka1:9092')
 msg_consumer = KafkaConsumer('chat', bootstrap_servers='kafka2:9092')
 img_consumer = KafkaConsumer('image', bootstrap_servers='kafka2:9092')
+pool = redis.ConnectionPool(host='kafka-chat-cache', port=6379, db=0)
+redis_c = redis.StrictRedis(connection_pool=pool)
 
-def receive_messages():
+def send_all_message(user):
+    print('send all')
+    for msg in redis_c.lrange('chat', 0, 100):
+        user.send(msg.decode('utf-8'))
+    print('-------end')
+
+def send_message():
     for msg in msg_consumer:
+        text = msg.value.decode('utf-8')
         for u in users:
-            u.send(msg.value.decode('utf-8'))
-threading.Thread(target=receive_messages).start()
+            u.send(text)
+        redis_c.rpush('chat', text)
+threading.Thread(target=send_message).start()
 
-def receive_image():
+def send_image():
     for msg in img_consumer:
-        for u in users:
-            u.send(msg.value.decode('utf-8'))
-threading.Thread(target=receive_image).start()
+        producer.send('chat', msg.value)
+            
+threading.Thread(target=send_image).start()
 
 @route('/')
 def index():
@@ -33,10 +45,11 @@ def statics(filename):
 @get('/chat', apply=[websocket])
 def chat(ws):
     users.add(ws)
+    send_all_message(ws)
     while True:
         msg = ws.receive()
         if msg is not None:
-            producer.send('chat', msg.encode('utf-8')) #.get(timeout=1)
+            producer.send('chat', msg.encode('utf-8')) #.get(timeout=1)            
         else:
             break
     users.remove(ws) # これ、thread-safeじゃないよね...
--- bin/dev.sh
+++ bin/dev.sh
@@ -2,6 +2,7 @@
 
 python3 /root/wait_for_tcp.py kafka1 9092 60
 python3 /root/wait_for_tcp.py kafka2 9092 60
+python3 /root/wait_for_tcp.py kafka-chat-cache 6379 60
 cd /root/chat
 pip3 install pipenv
 pipenv install

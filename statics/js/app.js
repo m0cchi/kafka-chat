9f48d8ade483f89ce96ebef8cb3345c852d7b56c
m0cchi <boom.boom.planet@gmail.com>
2018-07-28 14:21:46 +0900

    remove

--- statics/js/app.js
+++ statics/js/app.js
@@ -6,31 +6,21 @@ $(document).ready(function() {
         messageLine.text(date.toUTCString() + ': ' + text);
         timeline.prepend(messageLine);
     }
-    var isClosed = true;
-    function open() {
-        ws = new WebSocket('ws://127.0.0.1:8080/chat');
-        ws.onopen = function(e) {
-            isClosed = false;
-            addMessage(new Date(), 'start');
-            ws.send('Hello!')
-        };
-        ws.onmessage = function(e) {
-            addMessage(new Date(),e.data);
-        };
-        ws.onclose = function(e) {
-            isClosed = true;
-            addMessage(new Date(), 'close');
-        };
-        $('#form button').click(function(){
-            let text = messageForm.val();
-            ws.send(text);
-            messageForm.val('');
-        })
-    }
-    (function autoopen() {
-        if (isClosed) {
-            open();
-        }
-        setTimeout(autoopen, 5000);
-    })();
+
+    ws = new WebSocket('ws://127.0.0.1:8080/chat');
+    ws.onopen = function(e) {
+        addMessage(new Date(), 'start');
+        ws.send('Hello!')
+    };
+    ws.onmessage = function(e) {
+        addMessage(new Date(),e.data);
+    };
+    ws.onclose = function(e) {
+        addMessage(new Date(), 'close');
+    };
+    $('#form button').click(function(){
+        let text = messageForm.val();
+        ws.send(text);
+        messageForm.val('');
+    })
 });
